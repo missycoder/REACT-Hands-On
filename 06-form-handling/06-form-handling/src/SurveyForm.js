3d521f8c73403c166fcbf69cb163be9da9c481bd
missycoder mylan <noreen.taha@outlook.com>
2024-03-25 04:28:26 +0000

    update radio button in 06-form-handling

--- 06-form-handling/06-form-handling/src/SurveyForm.js
+++ 06-form-handling/06-form-handling/src/SurveyForm.js
@@ -1,9 +1,10 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 export default function SurveyForm() {
     // 1. setup hooks
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
+    const [color, setColor] = useState("red");
     // 2. initialization
 
     // for the onChange event handler, React
@@ -16,23 +17,51 @@ export default function SurveyForm() {
         setEmail(event.target.value);
     }
 
+    const handleColorChange = (event) => {
+        setColor(event.target.value)
+    }
+
     // 3. return JSX
     return (<>
-    <div>
-        <label>Name</label>
-        <input type="text" 
-               className="form-control"
-               value={name}
-               onChange={handleNameChange}
-        />
-    </div>
-    <div>
-        <label>Email</label>
-        <input type="text" 
-               className="form-control"
-               value={email}
-               onChange={handleEmailChange}
-        />
-    </div>
+        <div>
+            <label>Name</label>
+            <input type="text"
+                className="form-control"
+                value={name}
+                onChange={handleNameChange}
+            />
+        </div>
+        <div>
+            <label>Email</label>
+            <input type="text"
+                className="form-control"
+                value={email}
+                onChange={handleEmailChange}
+            />
+        </div>
+        <div>
+            <label>Favorite Color</label>
+            <input type="radio"
+                name="color"
+                value="red"
+                className="ms-1 me-1"
+                checked={color === "red"}
+                onChange={handleColorChange}
+            /><label>Red</label>
+            <input type="radio"
+                name="color"
+                value="blue"
+                className="ms-1 me-1"
+                checked={color === "blue"}
+                onChange={handleColorChange}
+            /><label>Blue</label>
+            <input type="radio"
+                name="color"
+                value="green"
+                className="ms-1 me-1"
+                checked={color === "green"}
+                onChange={handleColorChange}
+            /><label>Green</label>
+        </div>
     </>)
 }
