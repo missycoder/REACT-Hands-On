5028ccc7ab109d47f03ad63f2eb8c2f73638bfd3
missycoder mylan <noreen.taha@outlook.com>
2024-04-01 03:38:14 +0000

    update 10

--- 10-single-page-navigation/src/App.js
+++ 10-single-page-navigation/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import "./App.css"
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import RecipeBook from './components/RecipeBook';
+import RecipeBook from './components/RecipeBook.js';
+// import RecipeBook from './components/RecipeBook.css';
 
 
 function App() {
---
+++ 10-single-page-navigation/src/components/RecipeBook.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from "react";
+import Listing from "./Listing";
+import AddNew from "./AddNew";
+import axios from "axios";
+
+export default function RecipeBook() {
+
+    const [active, setActive] = useState('listing');
+
+    const [listingData, setListingData] = useState();
+
+    const [newTitle, setNewTitle] = useState("");
+    const [newIngredients, setNewIngredients] = useState("");
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    function renderContentChoice() {
+        if (active === "listing") {
+            return (
+                <React.Fragment>
+                    <Listing listingData={listingData} />
+                </React.Fragment>
+            )
+        } else if (active === "add") {
+            return (
+                <React.Fragment>
+                    <AddNew newTitle={newTitle}
+                        setNewTitle={setNewTitle}
+                        newIngredients={newIngredients}
+                        setNewIngredients={setNewIngredients}
+                        addNew={addNew}
+                    />
+                </React.Fragment>
+            )
+        }
+    }
+
+
+    let url = "https://3000-missycoder-react-6e2rwp8tpe5.ws-us110.gitpod.io"
+
+    async function addNew() {
+
+        const dataToPush = {
+            "_id": listingData.length + 1,
+            "title": newTitle,
+            "ingredients": newIngredients.split(",")
+        }
+
+        const updatedListing = [...listingData, dataToPush]
+
+        console.log("This is updated listing", updatedListing);
+
+        setListingData(updatedListing);
+        setActive("listing");
+
+        // This portion to update the DB instead, based on the demo endpoint
+
+        const payloadToDB = {
+            "title": newTitle,
+            "ingredients": newIngredients.split(",")
+        }
+
+        await axios.post(url + "/recipes", payloadToDB);
+
+    }
+
+    async function fetchData() {
+        let response = await axios.get(url + "/recipes");
+        console.log("this is response.data", response.data);
+        setListingData(response.data);
+    }
+
+    return (
+        <React.Fragment>
+            <div className="container">
+                <ul className="nav nav-tabs">
+                    <li className="nav-item">
+                        <button className="nav-link active"
+                            aria-current="page"
+                            onClick={() => setActive("listing")}
+                        >
+                            Recipe
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button className="nav-link"
+                            onClick={() => setActive("add")}
+                        >
+                            Add New
+                        </button>
+                    </li>
+                </ul>
+                {renderContentChoice()}
+            </div>
+        </React.Fragment>
+    )
+}
